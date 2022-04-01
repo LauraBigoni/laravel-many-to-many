2dafd36f94f0a613ccdd09f5bc4725b1cfa1de79
Laura Bigoni <laurabigoni.92@gmail.com>
2022-04-01 15:51:12 +0200

    changed name of the function format date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,7 +32,7 @@ class Post extends Model
         return $this->belongsToMany('App\Models\Tag');
     }
 
-    public function getUpdatedAt($date, $format = 'd-m-Y H:i:s')
+    public function getFormattedDate($date, $format = 'd-m-Y H:i:s')
     {
         return Carbon::create($this->$date)->format($format);
     }
